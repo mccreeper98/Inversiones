942658c4fd315d9718fc508a5932f79152211b61
Unknown <antxtolentino@gmail.com>
2017-06-23 10:18:48 -0500

    DB, proyectos
    Los proyectos se consumen desde la base de datos

--- proyecto/view/proyectosIndex.view.php
+++ proyecto/view/proyectosIndex.view.php
@@ -1,57 +1,39 @@
+<?php
+
+  try{
+  require 'conn.php';
+
+  $queryProyect = "SELECT proyecto.Nombre,proyecto.Des,images.img FROM proyecto INNER JOIN images ON proyecto.Nombre = images.proyecto";
+  $proyect = $conn->query($queryProyect);
+
+  foreach ($proyect as $p ) {
+?>    
+ 
 <div class="row">
 
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image">
-            <img src="img/example1.jpg">
-            <span class="card-title">Card Title</span>
+            <img src="<?php echo $p['img']?>">
+            <span class="card-title"><?php echo $p['Nombre']?></span>
           </div>
           <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+            <p><?php echo $p['Des']?></p>
             </div>
             <div class="card-action">
             <a href="#">VER MÁS</a>
             </div>
           </div>
         </div>
+      
 
+<?php
+ }
+}catch(Exeption $e){
 
+  echo "Error".$e."";
 
-        <div class="col s12 m4">
-          <div class="card">
-            <div class="card-image">
-              <img src="img/example2.jpg">
-              <span class="card-title">Card Title</span>
-            </div>
-            <div class="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div class="card-action">
-              <a href="#">VER MÁS</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col s12 m4">
-            <div class="card">
-              <div class="card-image">
-                <img src="img/example3.jpg">
-                <span class="card-title">Card Title</span>
-              </div>
-              <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively.</p>
-                </div>
-                <div class="card-action">
-                <a href="#">VER MÁS</a>
-                </div>
-              </div>
-            </div>
-
-             <div class="col s12 center" style="padding: 10px;">
-                <h5><a href="proyectos/index.html" class="negro"><strong><u>Ver todos los proyectos</u></strong></a></h5>
-             </div> 
-
-          </div>
+}
+?>
+</div>  
+          
