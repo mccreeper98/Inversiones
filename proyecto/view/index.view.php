cd53392496358d9db19ad8d939af6bd745219825
Unknown <antxtolentino@gmail.com>
2017-06-22 23:23:59 -0500

    nueva base de datos

--- proyecto/view/index.view.php
+++ proyecto/view/index.view.php
@@ -153,63 +153,7 @@
       </div>
     </center>
 
-    <div class="row">
-
-      <div class="col s12 m4">
-        <div class="card">
-          <div class="card-image">
-            <img src="img/example1.jpg">
-            <span class="card-title">Card Title</span>
-          </div>
-          <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-            <a href="#">VER MÁS</a>
-            </div>
-          </div>
-        </div>
-
-
-
-        <div class="col s12 m4">
-          <div class="card">
-            <div class="card-image">
-              <img src="img/example2.jpg">
-              <span class="card-title">Card Title</span>
-            </div>
-            <div class="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div class="card-action">
-              <a href="#">VER MÁS</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col s12 m4">
-            <div class="card">
-              <div class="card-image">
-                <img src="img/example3.jpg">
-                <span class="card-title">Card Title</span>
-              </div>
-              <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively.</p>
-                </div>
-                <div class="card-action">
-                <a href="#">VER MÁS</a>
-                </div>
-              </div>
-            </div>
-
-             <div class="col s12 center" style="padding: 10px;">
-                <h5><a href="proyectos/index.html" class="negro"><strong><u>Ver todos los proyectos</u></strong></a></h5>
-             </div> 
-
-          </div>
+    <?php require 'view/proyectosIndex.view.php'; ?>
 
         </div>
 
