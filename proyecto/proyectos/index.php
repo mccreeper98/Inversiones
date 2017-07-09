a5a033bf5efd0f4f590bc6441493ce76eed2a2ee
Bruno Giovanni Gomez <br1_gio@hotmail.com>
2017-07-09 03:03:32 -0500

    Mejora de estilos en pagina individual del proyecto

--- proyecto/proyectos/index.php
+++ proyecto/proyectos/index.php
@@ -2,18 +2,18 @@
 <html>
 <head>
 	<title>Invertir</title>
-	  <meta charset="utf-8">
-  <!--Import Google Icon Font-->
-  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!--Import materialize.css-->
-  <link rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
-  <link rel="stylesheet" href="../css/master.css">
-  <link rel="stylesheet" href="../css/animate.css">
-  <!--Import jQuery before materialize.js-->
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script type="text/javascript" src="../js/materialize.min.js"></script>
-  <!--Let browser know website is optimized for mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <meta charset="utf-8">
+ <!--Import Google Icon Font-->
+ <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ <!--Import materialize.css-->
+ <link rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
+ <link rel="stylesheet" href="../css/master.css">
+ <link rel="stylesheet" href="../css/animate.css">
+ <!--Import jQuery before materialize.js-->
+ <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <script type="text/javascript" src="../js/materialize.min.js"></script>
+ <!--Let browser know website is optimized for mobile-->
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
 
@@ -44,18 +44,18 @@
     </nav>
   </div>
 
-    <main style="padding-top: 80px">
+  <main style="padding-top: 80px">
 
 
-   	<div class="row">
+    <div class="row">
 
-   	<center><h3 style="margin-top: 20px;margin-bottom: 50px;">Elige el proyecto en el cual quieres invertir:</h3></center>	
-   	<div class="col s12 acentobg center" style="margin-bottom: 35px">
-   		<h4 class="white-text"><b>PARA INVERTIR</b></h4>
-   	</div>
-    <div class="divider col s12" style="margin-bottom: 30px"></div>
-          <div class="container">
-             <div class="col s12 m4">
+      <center><h3 style="margin-top: 20px;margin-bottom: 50px;">Elige el proyecto en el cual quieres invertir:</h3></center>	
+      <div class="col s12 acentobg center" style="margin-bottom: 35px">
+       <h4 class="white-text"><b>PARA INVERTIR</b></h4>
+     </div>
+     <div class="divider col s12" style="margin-bottom: 30px"></div>
+     <div class="container">
+       <div class="col s12 m4">
         <div class="card">
           <div class="card-image">
             <img src="../img/example1.jpg">
@@ -66,7 +66,7 @@
               I am convenient because I require little markup to use effectively.</p>
             </div>
             <div class="card-action">
-            <a href="proyecto">VER MÁS</a>
+              <a class="btn waves-effect waves-light negro col s12 animated" href="proyecto">VER MÁS</a><br><br>
             </div>
           </div>
         </div>
@@ -84,7 +84,7 @@
                 I am convenient because I require little markup to use effectively.</p>
               </div>
               <div class="card-action">
-              <a href="proyecto">VER MÁS</a>
+                <a class="btn waves-effect waves-light negro col s12 animated" href="proyecto">VER MÁS</a><br><br>
               </div>
             </div>
           </div>
@@ -100,55 +100,21 @@
                   I am convenient because I require little markup to use effectively.</p>
                 </div>
                 <div class="card-action">
-                <a href="proyecto">VER MÁS</a>
+                  <a class="btn waves-effect waves-light negro col s12 animated" href="proyecto">VER MÁS</a><br><br>
                 </div>
               </div>
             </div>
           </div>
 
-           <div class="col s12 amarillobg center" style="margin-bottom: 35px">
-   				<h4 class="white-text"><b>Proyectos terminados</b></h4>
-   			</div>
+          <div class="col s12 amarillobg center" style="margin-bottom: 35px">
+           <h4 class="white-text"><b>Proyectos terminados</b></h4>
+         </div>
 
-   		 <div class="container">
-          <div class="col s12 m4">
-        <div class="card">
-          <div class="card-image">
-            <img src="../img/example4.jpg">
-            <span class="card-title">Card Title</span>
-          </div>
-          <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-            <a href="proyecto">VER MÁS</a>
-            </div>
-          </div>
-        </div>
-
-
-
-        <div class="col s12 m4">
-          <div class="card">
-            <div class="card-image">
-              <img src="../img/example5.jpg">
-              <span class="card-title">Card Title</span>
-            </div>
-            <div class="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div class="card-action">
-              <a href="proyecto">VER MÁS</a>
-              </div>
-            </div>
-          </div>
-
+         <div class="container">
           <div class="col s12 m4">
             <div class="card">
               <div class="card-image">
-                <img src="../img/example6.jpg">
+                <img src="../img/example4.jpg">
                 <span class="card-title">Card Title</span>
               </div>
               <div class="card-content">
@@ -156,36 +122,70 @@
                   I am convenient because I require little markup to use effectively.</p>
                 </div>
                 <div class="card-action">
-                <a href="proyecto">VER MÁS</a>
+                  <a class="btn waves-effect waves-light negro col s12 animated" href="proyecto">VER MÁS</a><br><br>
                 </div>
               </div>
-            </div> 
-       </div>
+            </div>
 
-          </div>
-    </main>
 
-<?php require 'footer.php'; ?> 
 
-   <!--Start menu-->
-    <script type="text/javascript">
-      $(".button-collapse").sideNav();
-    </script>
-    <!--End menu-->
+            <div class="col s12 m4">
+              <div class="card">
+                <div class="card-image">
+                  <img src="../img/example5.jpg">
+                  <span class="card-title">Card Title</span>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                  </div>
+                  <div class="card-action">
+                    <a class="btn waves-effect waves-light negro col s12 animated" href="proyecto">VER MÁS</a><br><br>
+                  </div>
+                </div>
+              </div>
 
-    <!--Start of Tawk.to Script-->
-      <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/59458844e9c6d324a4735eec/default';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-        })();
-      </script>
-      <!--End of Tawk.to Script-->
+              <div class="col s12 m4">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="../img/example6.jpg">
+                    <span class="card-title">Card Title</span>
+                  </div>
+                  <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                      I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                      <a class="btn waves-effect waves-light negro col s12 animated" href="proyecto">VER MÁS</a><br><br>
+                    </div>
+                  </div>
+                </div> 
+              </div>
 
-</body>
-</html>
+            </div>
+          </main>
+
+          <?php require 'footer.php'; ?> 
+
+          <!--Start menu-->
+          <script type="text/javascript">
+            $(".button-collapse").sideNav();
+          </script>
+          <!--End menu-->
+
+          <!--Start of Tawk.to Script-->
+          <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/59458844e9c6d324a4735eec/default';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          </script>
+          <!--End of Tawk.to Script-->
+
+        </body>
+        </html>
